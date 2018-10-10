6f8feae45969bc23353d39d2a41dd5d58a21caaf
Justin <32234123+jdelfin16@users.noreply.github.com>
2018-10-10 15:35:37 -0700

    Update challenge/index.php

---
+++ challenge/index.php
@@ -0,0 +1,13 @@
+<?php
+
+	include "../rest.php";
+	$request = new RestRequest();
+	$method = $request->getRequestType();
+	$request_vars = $request->getRequestVariables();
+
+	$response = $request_vars;
+	$response["service"] = "challenge";
+	$response["method"] = $method;
+
+	$echo json_encode($response);
+?>
